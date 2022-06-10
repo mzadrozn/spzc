d0d87f862a6b572307a763d12de02558522f7eb7
mzadrozn <zadrozny.mateusz@outlook.com>
2022-06-10 21:47:31 +0200

    Add C# endpoint with Dockerfile

---
+++ spzcapp/spzcapp/Controllers/LongController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace zzsnapp.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class LongController : ControllerBase
+  {
+    private readonly ILogger<LongController> _logger;
+    private readonly int _sleepTime = 3500;
+    public LongController(ILogger<LongController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet(Name = "GetLong")]
+    public String Get()
+    {
+      Thread.Sleep(_sleepTime);
+      return ".NET long call";
+    }
+  }
+}
---
+++ spzcapp/spzcapp/Controllers/ShortController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+
+namespace zzsnapp.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ShortController : ControllerBase
+  {
+    private readonly ILogger<ShortController> _logger;
+    public ShortController(ILogger<ShortController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet(Name = "GetShort")]
+    public String Get()
+    {
+      return ".NET short call";
+    }
+  }
+}
